b18e6dc5bf464774219fd04495888dd2e04a47d9
emrekrn <136122266+emrekrn@users.noreply.github.com>
2024-03-12 22:02:20 +0100

    Static todoState has been created to test functionality

---
+++ src/utils/TodosState.js
@@ -0,0 +1,68 @@
+export default [
+	{
+		id: 1,
+		todoText:
+			"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange",
+		status: 'not started',
+	},
+	{
+		id: 2,
+		todoText:
+			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum',
+		status: 'not started',
+	},
+	{
+		id: 3,
+		todoText:
+			'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney Colleg',
+		status: 'not started',
+	},
+	{
+		id: 4,
+		todoText:
+			"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going",
+		status: 'not started',
+	},
+	{
+		id: 5,
+		todoText:
+			"Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+		status: 'not started',
+	},
+	{
+		id: 6,
+		todoText:
+			"Lorem Ipsum has been the industry's standard dummy text ever since the 150",
+		status: 'not started',
+	},
+	{
+		id: 7,
+		todoText:
+			'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et M',
+		status: 'not started',
+	},
+	{
+		id: 8,
+		todoText:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum, ex in fringilla mollis, nibh diam imperdiet lectus, vitae malesuada purus elit vitae ligula. ',
+		status: 'not started',
+	},
+	{
+		id: 9,
+		todoText:
+			"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going",
+		status: 'not started',
+	},
+	{
+		id: 10,
+		todoText:
+			'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et M',
+		status: 'not started',
+	},
+	{
+		id: 11,
+		todoText:
+			"Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+		status: 'not started',
+	},
+];
