35a387c6ce29520c621604b5a19d2deb21703188
emrekrn <136122266+emrekrn@users.noreply.github.com>
2024-03-12 22:02:56 +0100

    Creating new todo and deleting an existing todo have been implemented.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,21 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
-export default function Todo() {
+export default function Todo({ id, todoText, status, handleDelete }) {
 	return (
 		<div className='mb-2 flex h-12 items-center justify-between rounded-md border border-neutral-500 px-2.5'>
-			<div className='h-12 overflow-hidden'>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae debitis
-				dicta dolore dolores ea id impedit incidunt ipsa laudantium, minima
-				nobis non obcaecati odit optio pariatur perspiciatis provident, quia
-				quibusdam quo soluta voluptas voluptatem voluptatibus voluptatum!
-				Accusantium magni ratione totam. Cupiditate delectus dolor impedit,
-				inventore ipsa modi molestiae nemo numquam!
-			</div>
+			<div className='h-12 overflow-hidden'>{todoText}</div>
 			<div className='flex gap-2'>
 				<button className='ml-auto aspect-square h-10 rounded border-2 border-gray-800 duration-100 ease-linear hover:bg-gray-800 hover:text-white'>
 					<FontAwesomeIcon icon={faPenToSquare} />
 				</button>
-				<button className='ml-auto aspect-square h-10 rounded border-2 border-red-800 duration-100 ease-linear hover:bg-red-800 hover:text-white'>
+				<button
+					className='ml-auto aspect-square h-10 rounded border-2 border-red-800 duration-100 ease-linear hover:bg-red-800 hover:text-white'
+					onClick={() => handleDelete(id)}
+				>
 					<FontAwesomeIcon icon={faTrash} />
 				</button>
 			</div>
--- src/components/TodoField.jsx
+++ src/components/TodoField.jsx
@@ -1,26 +1,63 @@
 import Todo from './Todo.jsx';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import todosData from '../utils/TodosState.js';
+import CreateModal from './ui/CreateModal.jsx';
+import uniqid from 'uniqid';
 
 export default function TodoField() {
+	const [todos, setTodos] = useState(todosData);
+	const [showCreateModal, setShowCreateModal] = useState(false);
+
+	const handleShowModal = () => {
+		setShowCreateModal((prev) => !prev);
+	};
+
+	const handleCreateTodo = (todoText) => {
+		setTodos((prev) => [
+			...prev,
+			{
+				id: uniqid(),
+				todoText,
+				status: 'not started',
+			},
+		]);
+		handleShowModal();
+	};
+
+	const handleDelete = (id) => {
+		console.log(id);
+		setTodos((prev) => prev.filter((todo) => todo.id !== id));
+	};
+
+	const todoElements = todos.map((todo) => (
+		<Todo
+			key={todo.id}
+			id={todo.id}
+			todoText={todo.todoText}
+			status={todo.status}
+			handleDelete={handleDelete}
+		/>
+	));
+
 	return (
 		<div className='m-auto h-96 w-7/12 self-center rounded-2xl bg-gray-200 p-4'>
-			<div className='mb-4 flex'>
-				<button className='ml-auto aspect-square h-10 rounded border-2 border-emerald-600 duration-100 ease-linear hover:bg-emerald-600 hover:text-white'>
+			{showCreateModal && (
+				<CreateModal
+					handleShowModal={handleShowModal}
+					handleCreateTodo={handleCreateTodo}
+				/>
+			)}
+			<div className='mb-3 flex border-b border-b-gray-500 pb-2'>
+				<button
+					className='ml-auto aspect-square h-10 rounded border-2 border-emerald-600 duration-100 ease-linear hover:bg-emerald-600 hover:text-white'
+					onClick={handleShowModal}
+				>
 					<FontAwesomeIcon icon={faPlus} />
 				</button>
 			</div>
-			<div className='h-5/6 overflow-scroll'>
-				<Todo />
-				<Todo />
-				<Todo />
-				<Todo />
-				<Todo />
-				<Todo />
-				<Todo />
-				<Todo />
-				<Todo />
-			</div>
+			<div className='h-5/6 overflow-scroll'>{todoElements}</div>
 		</div>
 	);
 }
